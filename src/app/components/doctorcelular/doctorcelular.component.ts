b0fea8ce372f367f6ff776344203b0d14032394f
hilarionht <trejo.hilarion@gmail.com>
2017-10-28 16:59:24 -0300

    header, body, nabvar

---
+++ src/app/components/doctorcelular/doctorcelular.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-doctorcelular',
+  templateUrl: './doctorcelular.component.html',
+  styles: []
+})
+export class DoctorcelularComponent  {
+
+  
+
+}
